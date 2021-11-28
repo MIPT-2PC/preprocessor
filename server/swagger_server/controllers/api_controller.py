4eadf712076bd1b946cc77c8b07dc98865d8652b
ProValdi <prokhorov.va@phystech.edu>
2021-11-29 00:57:38 +0300

    develop make server work with postman

--- server/swagger_server/controllers/api_controller.py
+++ server/swagger_server/controllers/api_controller.py
@@ -1,10 +1,14 @@
 import connexion
 import six
+import operator
 
+from swagger_server.encoder import JSONEncoder
 from swagger_server.models.nums import Nums  # noqa: E501
 from swagger_server.models.results import Results  # noqa: E501
 from swagger_server import util
 
+results = {}
+from ..config import *
 
 def get_results():  # noqa: E501
     """Get previous results
@@ -14,7 +18,10 @@ def get_results():  # noqa: E501
 
     :rtype: List[Results]
     """
-    return 'do some magic!'
+    try:
+        return ServerDictInst.getAll(), 200
+    except:
+        return [], 404
 
 
 def operate(body=None):  # noqa: E501
@@ -27,6 +34,26 @@ def operate(body=None):  # noqa: E501
 
     :rtype: List[Nums]
     """
+
+    operations = {
+        "+": operator.add,
+        "*": lambda x, y: x * y,
+        "-": operator.sub
+    }
+
     if connexion.request.is_json:
         body = Nums.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+
+        num1 = body.num1
+        num2 = body.num2
+
+        try:
+            result = operations[body.operation](num1, num2)
+            input = str(num1) + " " + body.operation + " " + str(num2)
+
+            resultsObject = Results(result, input)
+            ServerDictInst.add(resultsObject)
+        except:
+            return {}, 500
+        return resultsObject, 200
+    return {}, 400
