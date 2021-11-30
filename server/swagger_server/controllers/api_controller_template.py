c19d0f6f8eb815360da15d1ac2db0df14309b799
ProValdi <ProValdi@users.noreply.github.com>
2021-11-30 18:14:12 +0000

    Apply automatic changes

---
+++ server/swagger_server/controllers/api_controller_template.py
@@ -0,0 +1,32 @@
+import connexion
+import six
+
+from swagger_server.models.nums import Nums  # noqa: E501
+from swagger_server.models.results import Results  # noqa: E501
+from swagger_server import util
+
+
+def get_results():  # noqa: E501
+    """Get previous results
+
+    retrieve results # noqa: E501
+
+
+    :rtype: List[Results]
+    """
+    return 'do some magic!'
+
+
+def operate(body=None):  # noqa: E501
+    """operate 2 numbers
+
+    operate 2 numbers with defined action # noqa: E501
+
+    :param body: Nums request body
+    :type body: dict | bytes
+
+    :rtype: List[Nums]
+    """
+    if connexion.request.is_json:
+        body = Nums.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
